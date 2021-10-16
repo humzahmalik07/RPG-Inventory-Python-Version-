3a2e03eaf15091a4651950d73d727461f1e30990
humzahmalik07 <89996008+humzahmalik07@users.noreply.github.com>
2021-10-16 02:06:45 +0000

    game.py modified

--- game.py
+++ game.py
@@ -29,31 +29,17 @@ You also have an option to quit the game. The option is provided in the menu.
 # This inventory is paired with specific characters
 inventory = {"Batman": {"Night Vision Goggles":
                         {"description": """Use the Night Vision Goggles to see
-                        in the dark and find your way""",
-                         "damage": 0, "protection": 0}},
+                        in the dark and find your way""",}},
              "Green Lantern": {"Power Ring":
                            {"description":
-                            "Use this ring as a flashlight to find their way",
-                            "damage": 0, "protection": 0}}
+                            "Use this ring as a flashlight to find their way",}}
              }
-
 
 def player_inventory(player, inventory):
     """Print out the inventory for the choosen character"""
-    protection_items = []
-    weapons = []
     for item in inventory[player]:
         description = inventory[player][item]["description"]
-        damage = inventory[player][item]["damage"]
-        protection = inventory[player][item]["protection"]
         print(f"{player}'s {item} - {description}")
-        print(f"damage: {damage}")
-        print(f"protection: {protection}")
-        if protection != 0 and damage == 0:
-            protection_items.append(item)
-        elif damage != 0:
-            weapons.append(item)
-    return protection_items, weapons
 
 # This takes the input for the character selection
 
